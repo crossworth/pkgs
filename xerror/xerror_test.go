35670c9792cf861f82c312d29c14cba81093a896
Pedro Henrique <system.pedrohenrique@gmail.com>
2024-09-08 00:26:22 -0300

    adds test for error

--- xerror/xerror_test.go
+++ xerror/xerror_test.go
@@ -1,6 +1,7 @@
 package xerror
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -9,4 +10,23 @@ import (
 func TestIsErrNotFound(t *testing.T) {
 	t.Parallel()
 	require.True(t, IsErrNotFound(MakeNotFoundError()))
+}
+
+func TestEncodeSliceErrParams(t *testing.T) {
+	t.Parallel()
+	err := MakeBadRequestError(
+		ErrParam("reason", "invalid argument"),
+		ErrParam("valueProvided", "-1"),
+		ErrParam("parseError", errors.New("parse error")),
+	)
+	require.Equal(t, "bad_request: parseError=parse error, reason=invalid argument, valueProvided=-1", err.Error())
+
+	err = MakeBadRequestError(
+		ErrParam("field", map[string]any{
+			"reason":        "invalid argument",
+			"valueProvided": "-1",
+			"parseError":    errors.New("parse error"),
+		}),
+	)
+	require.Equal(t, "bad_request: field=map[parseError:parse error reason:invalid argument valueProvided:-1]", err.Error())
 }
